7169960765841c21106260fe3b4dc9af6d0b6cae
Dominick Millner <dominick.millner@gmail.com>
2022-06-23 16:56:58 -0400

    add profit_and_loss report endpoint

--- main.py
+++ main.py
@@ -670,3 +670,244 @@ async def delete_journal_entry(journal_entry_id: str):
         return {"message": f"Journal Entry with id {journal_entry_id} has been deleted"}
     else:
         raise HTTPException(status_code=404, detail="Journal Entry not found")
+
+
+@app.get("/reports/profit_and_loss", tags=["Reports"])
+async def query_profit_and_loss():
+    profit_and_loss = {
+        "Header": {
+            "Customer": "1",
+            "ReportName": "ProfitAndLoss",
+            "Option": [
+                {
+                    "Name": "AccountingStandard",
+                    "Value": "GAAP"
+                },
+                {
+                    "Name": "NoReportData",
+                    "Value": "false"
+                }
+            ],
+            "ReportBasis": "Accrual",
+            "StartPeriod": "2022-06-01",
+            "Currency": "USD",
+            "EndPeriod": "2022-06-30",
+            "Time": "2022-03-03T13:00:18-08:00",
+            "SummarizeColumnsBy": "Total"
+        },
+        "Rows": {
+            "Row": [
+                {
+                    "Header": {
+                        "ColData": [
+                            {
+                                "value": "Income"
+                            },
+                            {
+                                "value": ""
+                            }
+                        ]
+                    },
+                    "Rows": {
+                        "Row": [
+                            {
+                                "Header": {
+                                    "ColData": [
+                                        {
+                                            "id": "45",
+                                            "value": "Landscaping Services"
+                                        },
+                                        {
+                                            "value": ""
+                                        }
+                                    ]
+                                },
+                                "Rows": {
+                                    "Row": [
+                                        {
+                                            "Header": {
+                                                "ColData": [
+                                                    {
+                                                        "id": "46",
+                                                        "value": "Job Materials"
+                                                    },
+                                                    {
+                                                        "value": ""
+                                                    }
+                                                ]
+                                            },
+                                            "Rows": {
+                                                "Row": [
+                                                    {
+                                                        "ColData": [
+                                                            {
+                                                                "id": "48",
+                                                                "value": "Fountains and Garden Lighting"
+                                                            },
+                                                            {
+                                                                "value": "275.00"
+                                                            }
+                                                        ],
+                                                        "type": "Data"
+                                                    },
+                                                    {
+                                                        "ColData": [
+                                                            {
+                                                                "id": "49",
+                                                                "value": "Plants and Soil"
+                                                            },
+                                                            {
+                                                                "value": "150.00"
+                                                            }
+                                                        ],
+                                                        "type": "Data"
+                                                    }
+                                                ]
+                                            },
+                                            "type": "Section",
+                                            "Summary": {
+                                                "ColData": [
+                                                    {
+                                                        "value": "Total Job Materials"
+                                                    },
+                                                    {
+                                                        "value": "425.00"
+                                                    }
+                                                ]
+                                            }
+                                        }
+                                    ]
+                                },
+                                "type": "Section",
+                                "Summary": {
+                                    "ColData": [
+                                        {
+                                            "value": "Total Landscaping Services"
+                                        },
+                                        {
+                                            "value": "425.00"
+                                        }
+                                    ]
+                                }
+                            },
+                            {
+                                "ColData": [
+                                    {
+                                        "id": "54",
+                                        "value": "Pest Control Services"
+                                    },
+                                    {
+                                        "value": "-100.00"
+                                    }
+                                ],
+                                "type": "Data"
+                            }
+                        ]
+                    },
+                    "type": "Section",
+                    "group": "Income",
+                    "Summary": {
+                        "ColData": [
+                            {
+                                "value": "Total Income"
+                            },
+                            {
+                                "value": "325.00"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "group": "GrossProfit",
+                    "type": "Section",
+                    "Summary": {
+                        "ColData": [
+                            {
+                                "value": "Gross Profit"
+                            },
+                            {
+                                "value": "325.00"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "Header": {
+                        "ColData": [
+                            {
+                                "value": "Expenses"
+                            },
+                            {
+                                "value": ""
+                            }
+                        ]
+                    },
+                    "type": "Section",
+                    "group": "Expenses",
+                    "Summary": {
+                        "ColData": [
+                            {
+                                "value": "Total Expenses"
+                            },
+                            {
+                                "value": ""
+                            }
+                        ]
+                    }
+                },
+                {
+                    "group": "NetOperatingIncome",
+                    "type": "Section",
+                    "Summary": {
+                        "ColData": [
+                            {
+                                "value": "Net Operating Income"
+                            },
+                            {
+                                "value": "325.00"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "group": "NetIncome",
+                    "type": "Section",
+                    "Summary": {
+                        "ColData": [
+                            {
+                                "value": "Net Income"
+                            },
+                            {
+                                "value": "325.00"
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "Columns": {
+            "Column": [
+                {
+                    "ColType": "Account",
+                    "ColTitle": "",
+                    "MetaData": [
+                        {
+                            "Name": "ColKey",
+                            "Value": "account"
+                        }
+                    ]
+                },
+                {
+                    "ColType": "Money",
+                    "ColTitle": "Total",
+                    "MetaData": [
+                        {
+                            "Name": "ColKey",
+                            "Value": "total"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    return profit_and_loss
